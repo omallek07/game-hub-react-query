06f5ef3237ed07c50194e3790f468928da8a15bf
Kevin O'Malley <omallek@gmail.com>
2023-07-13 08:20:40 -0400

    Add entities for interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,5 @@
 import { Card, CardBody, Heading, HStack, Image } from '@chakra-ui/react';
-import { Game } from '../hooks/useGames';
+import { Game } from '../entities/Game';
 import { Link } from 'react-router-dom';
 import getCroppedImageUrl from '../services/image-url';
 import CriticScore from './CriticScore';
